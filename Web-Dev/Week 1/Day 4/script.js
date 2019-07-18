f659acce2f400bc54c7120213ae187c3f0171759
ilumii <listeven073@gmail.com>
2019-07-18 19:06:39 -0400

    completed DOM Challenges 1

--- Web-Dev/Week 1/Day 4/script.js
+++ Web-Dev/Week 1/Day 4/script.js
@@ -1,7 +1,9 @@
 // gets the element container
 document.getElementById("container");
+
 // using queryselector
 document.querySelector("#container");
+
 // getting all itemswith class name second
 var x = document.getElementsByClassName("second");
 // for(var i = 0; i<x.length;i++){
@@ -9,6 +11,36 @@ var x = document.getElementsByClassName("second");
 // }
 
 //getting all Select a list item with a class of third, but only the list item inside of the ol tag.
-var x = document.getElementsByTagName("OL")[0];
-x.getElementsByClassName("third")[0];
-//
+var y = document.getElementsByTagName("OL")[0];
+y.getElementsByClassName("third")[0];
+
+//giving the section with id of container a textbox hello
+var textbox = document.createElement("textarea");
+textbox.innerHTML = "Hello!";
+var items = document.getElementById("container");
+items.insertBefore(textbox, items.childNodes[0]);
+
+//add the class main to the div with a class of footer
+var y= document.getElementsByClassName("footer")[0];
+y.classList.add("main");
+
+//remove class main on div with class of footer
+y.classList.remove("main");
+
+//creating new element li
+var li = document.createElement("li");
+//gave it text "four"
+li.innerHTML ="four";
+//appended it as a child to UL element
+document.getElementsByTagName("UL")[0].appendChild(li);
+
+//looping li for green background
+var ol_Elements = document.getElementsByTagName("OL");
+for(var i = 0; i<ol_Elements.length;i++){
+    ol_Elements[i].style.backgroundColor = "green"
+}
+
+//removed footer classname
+document.getElementsByClassName("footer")[0].remove();
+
+
